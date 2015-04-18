af85d7be94d2ff01953a290bec7640fb1694e8e5
Drako0 <drako_0@hotmail.com>
2015-04-18 11:59:18 -0500

    3

--- js/archivos.js
+++ js/archivos.js
@@ -4,29 +4,29 @@ var archivos = {
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, archivos.accesoAlSistema, archivos.error);
     },
     accesoAlSistema: function(sa){//Recibe el sistema de archivos
-        sa.root.getFile('practica.txt', {create: true, exclusive: false}, archivos.accesoArchivo, Archivos.error);
+        sa.root.getFile('practica.txt',{create: true, exclusive: false},archivos.accesoArchivo,archivos.error);
     },
-    accesoArchivo: function(){//Recibe la entrada del archivo
-        ea.crateWriter(archivos.escritor, archivos.error);
+    accesoArchivo: function(ea){//Recibe la entrada del archivo
+        ea.createWriter(archivos.escritor,archivos.error);
     },
-    escritor: function(e){//obtenemos el escritor como objeto
+    escritor: function(e){//Obtenemos el escritor como objeto
         var cont = $('#aSend').val();
         e.write(cont);
         e.onwriteend = function(evt){
-            navigator.notification.alert('El archivo fue creado satisfactoriamente', null, 'Archivos', 'Aceptar');
+            navigator.notification.alert('El archivo fue creado satisfactoriamente',null,'Arhivos','Aceptar');
         }
     },
     error: function(err){
-        navigator.notification.alert('Error: '+err.code,null,'Archivos', '¿Ya qué?');
+        navigator.notification.alert('Error: '+err.code,null,'Archivos','¿Ya qué?');
     },
-    leer: function(sa){
-        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, archivos.accesoSistema, archivos.error);        
+    leer: function(){
+        window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, archivos.accesoSistema, archivos.error);
     },
     accesoSistema: function(sa){
         sa.root.getFile('practica.txt',null,archivos.accesoFile,archivos.error);
     },
     accesoFile: function(ea){
-        ea.file(archivos.lector, archivos.error);    
+        ea.file(archivos.lector,archivos.error);
     },
     lector: function(a){
         var lector = new FileReader();
@@ -34,6 +34,5 @@ var archivos = {
         lector.onloadend = function(evt){
             $('#aGet').text(evt.target.result);
         }
-        
     }
 };
